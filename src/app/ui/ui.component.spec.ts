34811f729637eb5d4c2b72dee58990144b894f0d
Ivan1224 <magis@Ivan.com>
2022-12-02 10:10:58 -0600

    create branch sqrt  #6

--- src/app/ui/ui.component.spec.ts
+++ src/app/ui/ui.component.spec.ts
@@ -1,47 +1,46 @@
-import { sqr } from "./ui.component"
+import { sqrt } from "./ui.component"
 
-describe('sqr unit tests', () => {
-    
-    it('Should compute 2 ^ 2 = 4', () => {
+describe('sqrt unit tests', () => {
+
+    it('Should compute sqrt (25) = 5', () => {
         // Pattern AAA (Arrange - Act - Assert)
 
         // Arrange
         let result = 0;
         
         // Act
-        result = sqr(2);
+        result = sqrt(25);
 
+        // Assert
+        expect(result).toBe(5);
+    })
+
+    it('Should compute sqrt (16) = 4', () => {
+        // Arrange
+        let result = 0;
+        // Act
+        result = sqrt(16);
         // Assert
         expect(result).toBe(4);
     })
 
-    it('Should compute 3 ^ 2 = 9', () => {
+    it('Should compute sqrt (9) = 3', () => {
         // Arrange
         let result = 0;
         // Act
-        result = sqr(3);
+        result = sqrt(9);
         // Assert
-        expect(result).toBe(9);
+        expect(result).toBe(3);
     })
 
-    it('Should compute 4 ^ 2 = 16', () => {
+    it('Should compute sqrt (4) = 2', () => {
         // Arrange
         let result = 0;
         // Act
-        result = sqr(4);
+        result = sqrt(4);
         // Assert
-        expect(result).toBe(16);
+        expect(result).toBe(2);
     })
 
-    it('Should compute 5 ^ 2 = 25', () => {
-        // Arrange
-        let result = 0;
-        // Act
-        result = sqr(5);
-        // Assert
-        expect(result).toBe(25);
-    })
-
-   
-
+ 
 })
