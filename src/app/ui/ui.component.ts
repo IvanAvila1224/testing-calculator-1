df6ace6ab5d5f1f8d4cc8b8438d870c97e756eae
Ivan1224 <magis@Ivan.com>
2022-12-02 09:52:09 -0600

    create branch addition #2

--- src/app/ui/ui.component.ts
+++ src/app/ui/ui.component.ts
@@ -1,30 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { addition } from "../addition/addition"
-
-@Component({
-  selector: 'app-ui',
-  templateUrl: './ui.component.html',
-  styleUrls: ['./ui.component.css']
-})
-export class UiComponent implements OnInit {
-
-  constructor() { }
-
-  result = 0;
-  operator1 = 0;
-  operator2 = 0;
-
-  ngOnInit() {
-  }
-
-  addition() {
-    let myresult = 0;
-    myresult = addition(this.operator1, this.operator2);
-    this.result = myresult;
-  }
-
-  substraction() {
-    alert("substraction here !");
-  }
-
+export function addition(oper1, oper2) {  
+  // Refactoring
+  
+  let sum = 0;
+  sum = oper1 + oper2
+  return sum;
 }
